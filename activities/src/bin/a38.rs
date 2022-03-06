a7e098a53d33a26e02d79e10f0faee6e5d7fd7c5
Evan Schoening <evan.schoening@gmail.com>
2022-03-06 09:35:14 -0800

    threads and multithreaded program

--- activities/src/bin/a38.rs
+++ activities/src/bin/a38.rs
@@ -8,6 +8,8 @@
 // Notes:
 // * Use the join function to wait for threads to finish
 
+use std::thread;
+
 fn msg_hello() -> &'static str {
     use std::time::Duration;
     std::thread::sleep(Duration::from_millis(1000));
@@ -26,4 +28,29 @@ fn msg_excited() -> &'static str {
     "!"
 }
 
-fn main() {}
+fn main() {
+    let m1 = thread::spawn(move || {
+        msg_hello()
+    });
+    let m2 = thread::spawn(move || {
+        msg_thread()
+    });
+    let m3 = thread::spawn(move || {
+        msg_excited()
+    });
+
+    // Join returns a result
+    // could use the expect funtion here rather than match
+    match m1.join() {
+        Ok(m) => println!("message1 {:?}", m),
+        Err(e) => println!("error of some sort{:?}", e),
+    };
+    match m2.join() {
+        Ok(m) => println!("message1 {:?}", m),
+        Err(e) => println!("error of some sort{:?}", e),
+    };
+    match m3.join() {
+        Ok(m) => println!("message1 {:?}", m),
+        Err(e) => println!("error of some sort{:?}", e),
+    };
+}
--- activities/src/bin/athread.rs
+++ activities/src/bin/athread.rs
@@ -19,11 +19,32 @@ fn t1(){
     b.join();
 
 }
-fn main() {
-    //t1();
+
+fn ex2() {
     let t: JoinHandle<usize> = thread::spawn(move || {
         thread::sleep(Duration::from_secs(1));
         42
     });
+
+    println!("waiting on thread");
+    match t.join() {
+        Ok(n) => println!("value: {}", n),
+        Err(e) => println!("Error, {:?}", e),
+    }
+}
+fn main() {
+    let data = vec!['a', 'b', 'c'];
+    let nd = thread::spawn(move || {
+        let data: Vec<char> = data.iter()
+        .map(|letter| letter.to_ascii_uppercase())
+        .collect();
+        data
+    });
+
+    match nd.join() {
+        Ok(n) => println!("{:?} value is", n),
+        Err(e) => println!("{:?} error", e)
+    }
+
    
 }
