9bfb826d14a05a65734cb0264c03f525ec2eceb6
Evan Schoening <evan.schoening@gmail.com>
2022-03-13 09:14:49 -0700

    Rc and Refcell

--- activities/src/bin/acons.rs
+++ activities/src/bin/acons.rs
@@ -1,9 +1,14 @@
 use crate::List::{Cons, Nil};
 use std::ops::Deref;
 use std::rc::Rc;
+use std::cell::{self, RefCell};
 
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    // an Rc and a Refcell allow for multiple borrows that can be mutated
+    // Rc is for multiple borrows
+    // refCell is for mutable borrows
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
@@ -23,10 +28,11 @@ impl<T> Deref for MyBox<T> {
     }
 }
 fn main() {
-    let list = Cons(1, 
+    /*let list = Cons(1, 
         Rc::new(Cons( 2, 
             Rc::new(Cons(3, 
                 Rc::new(Nil))))));
+    */
     
     let x = 8;
     let y = MyBox::new(x);
@@ -34,9 +40,21 @@ fn main() {
     assert_eq!(8, x);
     // * is a dereference in Rust
     assert_eq!(8, *y);
+    let value = Rc::new(RefCell::new(5));
 
-    let b = (3, Rc::new(&list));
-    let c = (4, Rc::new(&list));
+    let ac = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let ab = Cons(Rc::new(RefCell::new(6)), Rc::clone(&ac));
+
+    let ad = Cons(Rc::new(RefCell::new(10)), Rc::clone(&ac));
+
+    *value.borrow_mut() += 10;
+
+    println!("ac after = {:?}", ac);
+    println!("ab after = {:?}", ab);
+    println!("ad after = {:?}", ad);
+
+
 
     
 }
--- activities/src/bin/ainterior_mutability.rs
+++ activities/src/bin/ainterior_mutability.rs
@@ -14,7 +14,7 @@ pub struct LimitTracker<'a, T: 'a + Messenger> {
     max: usize,
 }
 
-impl<'a, T> LimitTracker<a', T> 
+impl<'a, T> LimitTracker<'a, T> 
     where T: Messenger {
     pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
         LimitTracker {
@@ -36,5 +36,37 @@ impl<'a, T> LimitTracker<a', T>
         } else if percentage_of_max >= 0.75 {
             self.messenger.send("Warning, used 75% of quota");
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger{ sent_messages: RefCell::new(vec![])}
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
     }
 }
