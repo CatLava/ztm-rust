3eaec7556cc637bf6746e45add0766f791fb0f73
CatLava <evan.schoening@gmail.com>
2022-04-24 07:25:12 -0700

    start of final project

---
+++ clipstash/src/lib/domain/clip/field/mod.rs
@@ -0,0 +1,11 @@
+mod clip_id: 
+pub use clip_id::ClipId,
+mod shortcode: 
+pub use shortcode::ShortCode,
+mod content: 
+pub use field::Content,
+mod title: field::Title,
+mod posted: field::Posted,
+mod expires: field::Expires,
+mod password: field::Password,
+mod hits: field::Hits,
---
+++ clipstash/src/lib/domain/clip/mod.rs
@@ -0,0 +1,18 @@
+// structure fields part of the clip
+pub mod field;
+
+// All around clip storage here
+// all accessed in filed module
+// serialize is part of serde crate and need to include
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Clip {
+    pub clip_id: field::ClipId,
+    pub shortcode: field::ShortCode,
+    pub content: field::Content,
+    pub title: field::Title,
+    pub posted: field::Posted,
+    pub expires: field::Expires,
+    pub password: field::Password,
+    pub hits: field::Hits,
+
+}
