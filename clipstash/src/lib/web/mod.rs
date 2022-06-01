f4bbbcb27f6687e939c2d535b0e6e7aa7345da52
CatLava <evan.schoening@gmail.com>
2022-06-01 14:55:42 -0700

    added the rocket package and configured

--- clipstash/src/lib/web/mod.rs
+++ clipstash/src/lib/web/mod.rs
@@ -1,3 +1,30 @@
 // ctx short for context
 pub mod ctx;
 pub mod renderer;
+
+#[derive(rocket::Responder)]
+pub enum PageError {
+    #[response(status = 500)]
+    Serialization(String),
+    #[response(status = 500)]
+    Render(String),
+    #[response(status = 404)]
+    NotFound(String),
+    #[response(status = 500)]
+    Internal(String),
+}
+
+// Error trait is not compatible with rocket
+// need to implement this manually 
+
+impl From<handlebars::RenderError> for PageError {
+    fn from(err: handlebars::RenderError) -> Self {
+        PageError::Render(format!("{}", err))
+    }
+}
+
+impl From<serde_json::Error> for PageError {
+    fn from(err: serde_json::Error) -> Self {
+        PageError::Serialization(format!("{}", err))
+    }
+}
